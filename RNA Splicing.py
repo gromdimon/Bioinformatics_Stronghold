45eb031583e622bda3466b4b35089143f6cba4a4
Dima Gromyko <grom.dima.grom@gmail.com>
2021-10-29 10:37:08 +0300

    Add files via upload

---
+++ RNA Splicing.py
@@ -0,0 +1,65 @@
+CODON_TBL = {
+    "UUU":"F", "UUC":"F", "UUA":"L", "UUG":"L",
+    "UCU":"S", "UCC":"S", "UCA":"S", "UCG":"S",
+    "UAU":"Y", "UAC":"Y", "UAA":"STOP", "UAG":"STOP",
+    "UGU":"C", "UGC":"C", "UGA":"STOP", "UGG":"W",
+    "CUU":"L", "CUC":"L", "CUA":"L", "CUG":"L",
+    "CCU":"P", "CCC":"P", "CCA":"P", "CCG":"P",
+    "CAU":"H", "CAC":"H", "CAA":"Q", "CAG":"Q",
+    "CGU":"R", "CGC":"R", "CGA":"R", "CGG":"R",
+    "AUU":"I", "AUC":"I", "AUA":"I", "AUG":"M",
+    "ACU":"T", "ACC":"T", "ACA":"T", "ACG":"T",
+    "AAU":"N", "AAC":"N", "AAA":"K", "AAG":"K",
+    "AGU":"S", "AGC":"S", "AGA":"R", "AGG":"R",
+    "GUU":"V", "GUC":"V", "GUA":"V", "GUG":"V",
+    "GCU":"A", "GCC":"A", "GCA":"A", "GCG":"A",
+    "GAU":"D", "GAC":"D", "GAA":"E", "GAG":"E",
+    "GGU":"G", "GGC":"G", "GGA":"G", "GGG":"G"
+}
+def splicing(dna, introns):
+    # Changing intron to empty space
+    for intron in introns:
+        dna = dna.replace(intron, '')
+    return dna
+
+def rna_protein(rna):
+    # Translation rna to protein
+    splitted_rna = []  # Making codon list
+    while rna:
+        splitted_rna.append(rna[:3])
+        rna = rna[3:]
+    protein = ''
+    for codon in splitted_rna:
+        protein += CODON_TBL[codon]
+    return protein
+
+def extract_sequence_from_input(file):
+    '''
+    Reading fasta format
+    :param fp: multiples string on different lines
+    :return: sequences as list
+    '''
+    sequences = []
+    result = ""
+    for line in file:
+        if ">" in line:
+            sequences.append(result)  # Appending sequence list with final seq
+            result = ""  # Initiation of a new seq
+        else:
+            if "\n" in line:
+                result += line[:len(line) - 1]  # Lengthening of sequence
+            else:
+                result += line
+    sequences.append(result)  # Appending list with last seq
+    sequences.remove('')
+    return sequences
+
+# Reading file
+file = open('test.txt')
+seqs = extract_sequence_from_input(file)
+dna = seqs[0]
+introns = seqs[1:]
+post_dna = splicing(dna, introns)
+rna = post_dna.replace('T', 'U')  # Making RNA from DNA
+protein = rna_protein(rna)
+print(protein)
